3aab8c51a562e149c7056b2f55204002cf8e25ce
Jens Hausherr <jens.hausherr@xing.com>
2014-07-17 20:02:59 +0200

    Add tests

--- jsonmerge-test.go
+++ jsonmerge-test.go
@@ -20,3 +20,38 @@ func TestDisjunctMerge(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestNoModMerge(t *testing.T) {
+
+	m1 := make(map[string]interface{})
+	m1["foo"] = 1
+	m1["bar"] = true
+
+	m2 := make(map[string]interface{})
+
+	result := merge(m1, m2)
+
+	if len(result) != len(m1) {
+		t.Fail()
+	}
+
+	if result["foo"] != m1["foo"] {
+		t.Fail()
+	}
+
+	if result["bar"] != m1["bar"] {
+		t.Fail()
+	}
+}
+
+func TestEmptySourceObject(t *testing.T) {
+	m1 := make(map[string]interface{})
+	m2 := make(map[string]interface{})
+	m2["foo"] = 42
+
+	result := merge(m1, m2)
+
+	if v, ok = result["foo"]; !ok {
+		t.Fail()
+	}
+}
